411bab8690903278150571fc8cc85a0886cc784a
EvilPhilin <kurdjaev_sergey@rambler.ru>
2024-08-20 10:16:17 +0200

    Added post component

---
+++ src/components/Post.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const props = defineProps(['title', 'text'])
+  // const title = ref('No title received!');
+  // const text = ref('No text received!');
+</script>
+
+<template>
+  <div class="post-wrapper">
+    <h2 class="post-title">{{ title }}</h2>
+    <p class="post-text">{{ text }}</p>
+  </div>
+</template>
+
+<style scoped>
+  .post-wrapper {
+    border-bottom: 1px solid;
+  }
+
+  .post-text {
+    font-size: 18px;
+  }
+</style>
--- src/views/FeedView.vue
+++ src/views/FeedView.vue
@@ -1,22 +1,34 @@
 <script setup>
   import { ref } from 'vue'
+  import Post from '@/components/Post.vue';
   const apiUrl = 'http://127.0.0.1:3001/posts'
   
-  const temp = ref('No data yet')
-  fetch(apiUrl).then((response) => {
-    if (response.ok) {
-      response.json().then((data) => temp.value = data)
-    }
-  })
+  // const temp = ref('No data yet')
+  // fetch(apiUrl).then((response) => {
+  //   if (response.ok) {
+  //     response.json().then((data) => temp.value = data)
+  //   }
+  // })
+
+  const testData = [ { "title": "Brand new post1", "text": "Wise thought1" },
+                     { "title": "Brand new post2", "text": "Wise thought2" },
+                     { "title": "Brand new post3", "text": "Wise thought3" } ]
 </script>
 
 <template>
-  <div>
-    <h1>Hello from FeedView</h1>
-    {{ temp }}
+  <div class="feed-wrapper">
+    <div class="posts-grid">
+      <Post v-for="(post, idx) in testData" :title="post.title" :text="post.text" :key="idx" />
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+  .feed-wrapper {
+    margin: auto 5% 5%;
+  }
+  .posts-grid {
+    display: block;
+    padding: 1% 2%;
+  }
 </style>
