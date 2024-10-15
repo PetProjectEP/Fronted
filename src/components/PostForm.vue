f0c9fdc09d17562ca2c0162b6a9b6c6db03989b0
EvilPhilin <kurdjaev_sergey@rambler.ru>
2024-10-15 10:08:26 +0200

    Pulled post form to distinct component

---
+++ src/components/PostForm.vue
@@ -0,0 +1,42 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const title = ref("")
+  const text = ref("")
+  defineExpose({title, text})
+</script>
+
+<template>
+  <div class="inputs">
+    <input v-model="title" placeholder="Name your emission..."/>
+    <textarea v-model="text" placeholder="Write down your powerful thoughts..."/>
+  </div>
+</template>
+
+<style scoped>
+  .inputs {
+    width: 100%;
+    text-align: center;
+  }
+
+  input {
+    font-size: 24px;
+    border-radius: 5px;
+    border-color: rgba(0, 0, 0, 0.2);
+    width: 70%;
+    height: 2em;
+    margin: 1.5em 0 0 0;
+    padding: 1px 10px;
+  }
+
+  textarea {
+    font-size: 24px;
+    border-radius: 5px;
+    border-color: rgba(0, 0, 0, 0.2);
+    width: 70%;
+    height: 10em;
+    margin: 1.5em 0 0 0;
+    padding: 1px 10px;
+    resize: none;
+  }
+</style>
--- src/views/CreatePostView.vue
+++ src/views/CreatePostView.vue
@@ -1,25 +1,21 @@
 <script setup>
   import router from '@/router'
+  import PostForm from "@/components/PostForm.vue"
   import { ref, onMounted } from 'vue'
   import { createPost } from '@/common/BackendCalls/PostServiceCalls';
 
-  const title = ref("")
-  const text = ref("")
-
-  const signInWindow = ref(null)
-  onMounted(() => signInWindow.value?.scrollIntoView({ behavior: 'smooth' }))
+  const form = ref(null)
+  const modalWindow = ref(null)
+  onMounted(() => modalWindow.value?.scrollIntoView({ behavior: 'smooth' }))
 </script>
 
 <template>
   <div class="modal-overlay">
-    <div class="modal-window" ref="signInWindow">
+    <div class="modal-window" ref="modalWindow">
       <div class="content">
-        <div class="inputs">
-          <input v-model="title" placeholder="Name your emission..."/>
-          <textarea v-model="text" placeholder="Write down your powerful thoughts..."/>
-        </div>
+        <PostForm ref="form"/>
         <div class="buttons">
-          <button class="submit-button" @click="createPost(title, text)">Polute!</button>
+          <button class="submit-button" @click="createPost(form.title, form.text)">Polute!</button>
           <button class="cancel-button" @click="router.push('/')">Cancel</button>
         </div>
       </div>
@@ -41,32 +37,6 @@
     margin: auto;
     justify-self: center;
     align-self: center;
-  }
-
-  .inputs {
-    width: 100%;
-    text-align: center;
-  }
-
-  input {
-    font-size: 24px;
-    border-radius: 5px;
-    border-color: rgba(0, 0, 0, 0.2);
-    width: 70%;
-    height: 2em;
-    margin: 1.5em 0 0 0;
-    padding: 1px 10px;
-  }
-
-  textarea {
-    font-size: 24px;
-    border-radius: 5px;
-    border-color: rgba(0, 0, 0, 0.2);
-    width: 70%;
-    height: 10em;
-    margin: 1.5em 0 0 0;
-    padding: 1px 10px;
-    resize: none;
   }
 
   .buttons {
